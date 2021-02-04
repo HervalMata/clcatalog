755ca651455ee93d0d3bc4dd94816174c3fd39ab
HervalMata <hervalmataamparo@gmail.com>
2021-02-04 18:57:13 -0300

    Database Seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Laços');
+INSERT INTO tb_category (name) VALUES ('Tiaras');
+INSERT INTO tb_category (name) VALUES ('Viseiras');
+INSERT INTO tb_category (name) VALUES ('Faixas');
