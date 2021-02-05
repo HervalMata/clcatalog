f1a0e4b1f3e710acd7e76c61e6ac15ee136dd14c
HervalMata <hervalmataamparo@gmail.com>
2021-02-05 15:15:33 -0300

    Product Entity, SQL Script

--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tb_category (name, createdAt) VALUES ('Laços', NOW());
-INSERT INTO tb_category (name, createdAt) VALUES ('Tiaras', NOW());
-INSERT INTO tb_category (name, createdAt) VALUES ('Viseiras', NOW());
-INSERT INTO tb_category (name, createdAt) VALUES ('Faixas', NOW());
