a2077f6412c1c9a711d6026da67a28d46bd27a57
HervalMata <hervalmataamparo@gmail.com>
2021-02-10 00:20:49 -0300

    H@ Security Configuration

--- backend/src/main/java/com/herval/clcatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/herval/clcatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.herval.clcatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -18,6 +21,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private static final String[] ADMIN = { "/users" };
 	
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 
 	@Override
@@ -27,6 +33,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
 			.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
